50010a517d071e086eb69118da9d5be7d486abe2
Paul Redmond <paulrredmond@gmail.com>
2015-12-20 09:43:41 -0700

    Install MariaDB

--- bin/bootsrap-ubuntu.sh
+++ bin/bootsrap-ubuntu.sh
@@ -41,6 +41,13 @@ sudo apt-get install -y \
 	mysql-client \
 	oracle-java8-installer
 
+# MariaDB
+sudo apt-get install software-properties-common
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+sudo add-apt-repository 'deb [arch=amd64,i386] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu trusty main'
+sudo apt-get update
+sudo apt-get install mariadb-server
+
 # Docker
 sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo touch /etc/apt/sources.list.d/docker.list
